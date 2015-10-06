6165d0a0be515fcf735ca51b22a5557bb9722c60
Ivana Drábiková <idrabikova@gmail.com>
2015-10-06 17:26:17 +0200

    Find

--- src/bvs/BST.java
+++ src/bvs/BST.java
@@ -14,16 +14,16 @@ public class BST {
     public BST() {
     }
     
-    public void insert(Node newRoot, int newKey) {
+    public void insert(Node node, int newKey) {
         Node newLeaf = new Person(newKey);
         if(this.root == null) {
-            root = newLeaf;
+            this.root = newLeaf;
         } 
         else {
-            Node root       = newRoot;
-            Node rightChild = root.getRightChilds();
-            Node leftChild  = root.getLeftChild();
-                if(newLeaf.isGreaterThan(root)) {
+            Node newRoot    = node;
+            Node rightChild = newRoot.getRightChilds();
+            Node leftChild  = newRoot.getLeftChild();
+                if(newLeaf.isGreaterThan(newRoot)) {
                     insert(rightChild, newKey);
                 }
                 else {
@@ -32,8 +32,30 @@ public class BST {
         }
     }
     
-    public Node find(int key) {
-        return root;
+    public Node find(Node node, int key) {
+        if(this.root==null) {
+            return null;
+        } 
+        else {
+            Node searchedNode = new Person(key);
+            Node newRoot    = node;
+            Node rightChild = newRoot.getRightChilds();
+            Node leftChild  = newRoot.getLeftChild();
+            if(searchedNode.isGreaterThan(newRoot)) {
+                if(rightChild!=null) {
+                    find(rightChild, key);
+                } else {
+                    return rightChild;
+                }
+            } 
+            else {
+                if(leftChild!=null) {
+                    find(leftChild, key);
+                } else {
+                    return leftChild;
+                }
+            }
+        }
     }
     
     public void inorder() {
